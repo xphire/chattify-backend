76fa28f29a1653e0698ce18e60c4e500f7b874d8
Abdulafeez Adetunmbi <xplicifire@gmail.com>
2025-02-12 04:19:51 +0100

    removed socket folder

--- src/sockets/sockets.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SocketsService } from './sockets.service';
-import { SocketsGateway } from './sockets.gateway';
-
-@Module({
-  providers: [SocketsGateway, SocketsService],
-})
-export class SocketsModule {}
